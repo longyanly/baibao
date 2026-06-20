eb8bb936bd48a0cc4b890ca323fd0d0a524c5909
longyanly <18986491820@163.com>
2026-06-20 13:39:03 +0800

    Add files via upload

---
+++ tipsData.js
@@ -0,0 +1,15 @@
+/**
+ * 使用小技巧（单页）
+ */
+const tipsData = [
+    {
+        id: 'tips',
+        name: '使用小技巧',
+        image: 'img/tc_chest.png',
+        features: [
+            '实用技巧 22 条：战斗、烹饪、建家、混沌、种地等',
+            '快速获得[[万物百宝]]、多模组懒人刷包裹等进阶思路',
+        ],
+        hasDetail: true,
+    },
+];
